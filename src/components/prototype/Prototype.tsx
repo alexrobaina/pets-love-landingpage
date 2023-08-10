35b9bda1caa9cc2be85a4b4c400eb431b8cf5623
alexrobaina <alexrobainaph@gmail.com>
2023-08-10 12:11:27 -0300

    navbar finished

--- src/components/prototype/Prototype.tsx
+++ src/components/prototype/Prototype.tsx
@@ -8,7 +8,7 @@ const Prototype = () => {
   const t = useTranslations('prototype')
 
   return (
-    <section>
+    <section id='prototype'>
       <div className="gap-20 grid-cols-1 center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6  sm:grid-cols-1">
         <div className="font-light  sm:text-lg text-primary-950">
           <h2 className="mb-4 text-4xl font-extrabold ">{t('title')}</h2>
