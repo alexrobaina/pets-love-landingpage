1e04a7428c3e1a217a3fb22d8de6bb9dc83daa61
alexrobaina <alexrobainaph@gmail.com>
2023-12-06 19:44:11 -0300

    fix errors deploys 7

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import Hero from '@/components/Hero'
 import { Features } from '@/components/Features'
-import { FeatureSections } from '@/components/FeaturesSections/FeatureSections'
+import { FeatureSections } from '@/components/FeaturesSections'
 import Collaborate from '@/components/Collaborate'
 import JointUp from '@/components/Joinup'
 import Donation from '@/components/Donation'
