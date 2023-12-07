3bc7acbf96569ceb2d2f835932a62b8d663a7d45
alexrobaina <alexrobainaph@gmail.com>
2023-12-07 11:34:13 -0300

    fix deploy

---
+++ src/components/FeaturesSections/components/Feature/index.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react'
+import Image, { StaticImageData } from 'next/image'
+import View from '@/hook/useView'
+
+interface Props {
+  content: any
+  order: boolean
+  flip?: boolean
+  img: StaticImageData
+}
+
+const Feature: FC<Props> = ({ flip, content, img, order }) => {
+  return (
+    <article
+      className={
+        order
+          ? ' flex flex-col md:flex-row-reverse mr-auto items-center py-8 mx-auto gap-5'
+          : ' flex flex-col md:flex-row mr-auto justify-center items-center py-8 mx-auto gap-5'
+      }
+    >
+      <View>
+        <div>
+          <Image
+            src={img}
+            width={650}
+            alt="mockup"
+            className={`${flip && 'transform -scale-x-100'}`}
+          />
+        </div>
+      </View>
+      <View>
+        <div>{content}</div>
+      </View>
+    </article>
+  )
+}
+
+export default Feature
---
+++ src/components/FeaturesSections/index.tsx
@@ -0,0 +1,142 @@
+import React from 'react'
+import Feature from './components/Feature'
+import { useTranslations } from 'next-intl'
+import View from '@/hook/useView'
+import { ManAdopter, Refugy, Vet } from '@/assets/images'
+
+export const FeaturesSections = () => {
+  const t = useTranslations('shelter')
+  const t1 = useTranslations('adopt')
+  const t2 = useTranslations('veterinary')
+  const t3 = useTranslations('voluntaries')
+  return (
+    <section id="features">
+      <Feature
+        flip
+        img={Refugy}
+        order={false}
+        content={
+          <article className="text-primary-950 mt-20">
+            <View>
+              <h1 className="mb-10 max-w-2xl text-2xl font-extrabold leading-none md:text-4xl xl:text-5xl ">
+                {t('shelterTitle')}
+              </h1>
+              <h3 className="mb-3 lg:mb-5 max-w-2xl text-md font-bold leading-none md:text-xl xl:text-md">
+                {t('shelterSubtitle1')}
+              </h3>
+            </View>
+            <View>
+              <p className="mb-4 max-w-2xl font-light  lg:mb-8 md:text-lg lg:text-xl ">
+                {t('shelterDescription1')}
+              </p>
+            </View>
+            <View>
+              <h3 className="mb-3 lg:mb-5 max-w-2xl text-md font-bold leading-none md:text-xl xl:text-md">
+                {t('shelterSubtitle2')}
+              </h3>
+            </View>
+            <View>
+              <p className="mb-4 max-w-2xl font-light  lg:mb-8 md:text-lg lg:text-xl ">
+                {t('shelterDescription2')}
+              </p>
+            </View>
+          </article>
+        }
+      />
+
+      <Feature
+        content={
+          <article className="text-primary-950">
+            <View direction={true}>
+              <h1 className="mb-10 max-w-2xl text-2xl font-extrabold leading-none md:text-4xl xl:text-5xl ">
+                {t1('adoptTitle')}
+              </h1>
+            </View>
+            <View direction={true}>
+              <p className="mb-4 max-w-2xl font-light  lg:mb-8 md:text-lg lg:text-xl ">
+                {t1('adoptDescription1')}
+              </p>
+            </View>
+            <View direction={true}>
+              <p className="mb-4 max-w-2xl font-light  lg:mb-8 md:text-lg lg:text-xl ">
+                {t1('adoptDescription2')}
+              </p>
+            </View>
+          </article>
+        }
+        img={ManAdopter}
+        order={true}
+      />
+      {/* VETERINARIAN */}
+      <Feature
+        content={
+          <article className="text-primary-950">
+            <View>
+              <h1 className="mb-10 max-w-2xl text-2xl font-extrabold leading-none md:text-4xl xl:text-5xl ">
+                {t2('veterinaryTitle')}
+              </h1>
+            </View>
+            <View>
+              <h3 className="mb-3 lg:mb-5 max-w-2xl text-md font-bold leading-none md:text-xl xl:text-md">
+                {t2('veterinarySubtitle1')}
+              </h3>
+            </View>
+            <View>
+              <p className="mb-4 max-w-2xl font-light  lg:mb-8 md:text-lg lg:text-xl ">
+                {t2('veterinaryDescription1')}
+              </p>
+            </View>
+            <View>
+              <h3 className="mb-3 lg:mb-5 max-w-2xl text-md font-bold leading-none md:text-xl xl:text-md">
+                {t2('veterinarySubtitle2')}
+              </h3>
+            </View>
+            <View>
+              <p className="mb-4 max-w-2xl font-light  lg:mb-8 md:text-lg lg:text-xl ">
+                {t2('veterinaryDescription2')}
+              </p>
+            </View>
+          </article>
+        }
+        img={Vet}
+        order={false}
+      />
+
+      {/* Volunteers are disable now */}
+
+      {/* <Feature
+        content={
+          <article className="text-primary-950">
+            <View direction={true}>
+              <h1 className="mb-10 max-w-2xl text-2xl font-extrabold leading-none md:text-4xl xl:text-5xl ">
+                {t3('voluntariesTitle')}
+              </h1>
+            </View>
+            <View direction={true}>
+              <h3 className="mb-3 lg:mb-5 max-w-2xl text-md font-bold leading-none md:text-xl xl:text-md">
+                {t3('voluntariesSubtitle1')}
+              </h3>
+            </View>
+            <View direction={true}>
+              <p className="mb-4 max-w-2xl font-light  lg:mb-8 md:text-lg lg:text-xl ">
+                {t3('voluntariesDescription1')}
+              </p>
+            </View>
+            <View direction={true}>
+              <h3 className="mb-3 lg:mb-5 max-w-2xl text-md font-bold leading-none md:text-xl xl:text-md">
+                {t3('voluntariesSubtitle2')}
+              </h3>
+            </View>
+            <View direction={true}>
+              <p className="mb-4 max-w-2xl font-light  lg:mb-8 md:text-lg lg:text-xl ">
+                {t3('voluntariesDescription2')}
+              </p>
+            </View>
+          </article>
+        }
+        img={Voluntarios}
+        order={true}
+      /> */}
+    </section>
+  )
+}
