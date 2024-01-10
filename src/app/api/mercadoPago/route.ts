05998ab102441b0e04b62b14ff5db81ee1c091be
Wayex <30770802+alexrobaina@users.noreply.github.com>
2024-01-10 08:01:29 -0300

    fix mercadopago (#35)

--- src/app/api/mercadoPago/route.ts
+++ src/app/api/mercadoPago/route.ts
@@ -9,13 +9,9 @@ mercadopago.configure({
 
 export async function POST(req: NextRequest, res: NextApiResponse) {
   try {
-    const request = await req.json();
-    const product = request.product;
-    const URL = process.env.HOST;
-
-    const data = {
-      productId: product.id
-    }
+    const request = await req.json()
+    const product = request.product
+    const URL = process.env.HOST
 
     const preference: CreatePreferencePayload = {
       items: [
@@ -23,7 +19,6 @@ export async function POST(req: NextRequest, res: NextApiResponse) {
           title: product.title,
           description: cutText(product.description),
           unit_price: parseInt(product.price),
-          picture_url: product?.image || '',
           quantity: 1,
         },
       ],
@@ -32,30 +27,30 @@ export async function POST(req: NextRequest, res: NextApiResponse) {
         success: `${URL}/paySuccess?productId=${product.id}&title=${product.title}&price=${product.inputValue}&payType=mercadopago`,
         failure: `${URL}/payError?productId=${product.id}&title=${product.title}&price=${product.inputValue}&payType=mercadopago`,
       },
-    };
+    }
 
-    const response = await mercadopago.preferences.create(preference);
+    const response = await mercadopago.preferences.create(preference)
 
     return new NextResponse(JSON.stringify(response), {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
       },
-    });
+    })
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error:', error)
     return new NextResponse(JSON.stringify({ error }), {
       status: 500,
       headers: {
         'Content-Type': 'application/json',
       },
-    });
+    })
   }
 }
 
-function cutText(texto: string) : string {
+function cutText(texto: string): string {
   if (texto.length > 200) {
-    return texto.substring(0, 200) + '...';  // Añade puntos suspensivos si el texto es más largo que 200 caracteres
+    return texto.substring(0, 200) + '...' // Añade puntos suspensivos si el texto es más largo que 200 caracteres
   }
-  return texto;
+  return texto
 }
